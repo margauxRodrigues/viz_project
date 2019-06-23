b4c3a70443cb7918fa7dc5de17ba415adcf0a17a
Colin Parellier <43640856+Colin75@users.noreply.github.com>
2019-06-23 13:25:42 +0200

    add France

--- Code/js/cartogram_fr.js
+++ Code/js/cartogram_fr.js
@@ -10,13 +10,18 @@ var svg = d3.select("#france")
         .attr("height",  containerHeight - 10 );
 
 
-d3.json('./Code//json/france.json', (error, world) => {
+
+d3.json('./Code/json/france.json', (error, france) => {
     if (error) throw error;
 
     const colorScale = d3.scaleOrdinal([...d3.schemeCategory20, ...d3.schemeCategory20b, ...d3.schemeCategory20c]);
+    console.log(france);
 
     Cartogram()
-        .topoJson(world)
+        .topoJson(france)
         .topoObjectName('countries')
-        .iterations(120);
+        .iterations(120)
+        .valFormatter(d3.format(".3s"))
+        .color('LightPink')
+        (document.getElementById('france'));
 });
