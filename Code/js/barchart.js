63c7c54187d958adb1d5912e36e7eabe2bd8d151
margauxRodrigues <margaux.rodrigues@telecom-paristech.fr>
2019-06-23 19:10:33 +0200

    Retrouve les donnees

--- Code/js/barchart.js
+++ Code/js/barchart.js
@@ -38,69 +38,75 @@ d3.csv("data/data_fr.csv")
     }
 });
 
- 
+var output;
+
 setTimeout(function(){
-  drawBarChart(data)
-  },1000);
+  output =
+  d3.rollups(
+    data,
+    xs => d3.sum(xs, x => x.y2015),
+    d =>  d.icd10_2
+  )
+  .map(([k, v]) => ({ Maladie: k, Value: v }))
+
+  drawBarChart(output)
+  },2000);
 
 
 function drawBarChart(data){
-    // Add X axis
-    var max = d3.max(data, function(d){ return d.y2015  ; });
-    var x = d3.scaleLinear()
-      .domain([0, max + 20])
-      .range([ 0, 280]);
-    svg.append("g")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+
+  var x = d3.scaleBand()
+          .range([0, containerWidth])
+          .padding(0.1);
+  var y = d3.scaleLinear()
+          .range([containerHeight, 0]);
+          
+  x.domain(data.map(function(d) { return d.Maladie; }));
+  y.domain([0, d3.max(data, function(d) { return d.Value; })]);
+    
+  // Add X axis
+    // var max = d3.max(data, function(d){ return d.Value  ; });
+    // console.log(max);
+    // var x = d3.scaleLinear()
+    //   .domain([0, max + 20])
+    //   .range([ 0, 280]);
+    
+    //   svg.append("g")
+    //     .call(d3.axisBottom(x))
+    //     .selectAll("text")
+    //     .attr("transform", "translate(-10,0)rotate(-45)")
+    //     .style("text-anchor", "end");
 
     // Y axis
-    console.log(d3.min(data, function(d){ return d.y2015; }))
-    var y = d3.scaleBand()
-      .domain(data.map(function(d) { return d.icd10_2; }))
-      .range([ 0, 2 * containerHeight - 20])
-      .padding(.5);
-    svg.append("g")
-      .call(d3.axisLeft(y))
+    // console.log(d3.min(data, function(d){ return d.Value; }))
+
+    // var y = d3.scaleBand()
+    //   .domain(data.map(function(d) { return d.Maladie; }))
+    //   .range([ 0, 2 * containerHeight - 20])
+    //   .padding(.5);
+
+    //   svg.append("g")
+    //   .call(d3.axisLeft(y))
+
     //Bars
     console.log('Barchart Call')
-    console.log(data)
-
-  /* var sum_par_maladie =
-  d3.rollups(
-      data,
-      xs => d3.sum(xs, x => x.y2015),
-      d => d.icd10_niv_2
-    )
-    .map(([k, v]) => ({ icd10_niv_2: k, y2015: v }))
-
-  console.log(sum_par_maladie)
-  */
- var output =
-    d3.rollups(
-        data,
-        xs => d3.sum(xs, x => x.y2015),
-        d => d.icd10_2
-      )
-      .map(([k, v]) => ({ Maladie: k, Value: v }))
 
     svg.selectAll("myRect")
       .data(data)
       .enter()
-      .selectAll("text")
-        .attr("transform", "translate(10,0)")
-        .style("text-anchor", "end")
+      // .selectAll("text")
+      //   .attr("transform", "translate(10,0)")
+      //   .style("text-anchor", "end")
       .append("rect")
       .attr("transform", "translate(0," + 35 + ")")
-      .attr("x", function(d) { return x(0); } )
-      .attr("y", function(d) { return y(d.icd10_2); })
-      .attr("width", function(d) { return output.Value; })
-      .attr("height", y.bandwidth() )
+      .attr("x", function(d) { return x(d.Maladie); } )
+      .attr("y", function(d) { return y(d.Value); })
+      .attr("width", x.bandwidth)
+      .attr("height", function(d) { return containerHeight - y(d.Value); })
+      
       .style("fill", "#666")
-  }
 
+    }
     // .attr("x", function(d) { return x(d.Country); })
     // .attr("y", function(d) { return y(d.Value); })
     // .attr("width", x.bandwidth())
