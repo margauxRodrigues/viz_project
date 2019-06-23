bb7813d35a78c7acab539ab5df225959732f8635
Colin <43640856+Colin75@users.noreply.github.com>
2019-06-23 10:08:45 +0200

    Add dependecies for cartogram

--- Code/js/cartogram.js
+++ Code/js/cartogram.js
@@ -1,363 +1,146 @@
-var parentDiv = document.getElementById("france")
+import { select as d3Select, event as d3Event } from 'd3-selection/d3-selection.min';
+import 'd3-selection/d3-selection.min';
+import {geoMercator} from 'd3-geo/d3-geo.min';
+import cartogramModule from 'topogram/cartogram';
+import Kapsule from 'kapsule/kapsule';
+import accessorFn from 'accessor-fn/accessor-fn';
+import 'd3/d3.min'
+
+var parentDiv = document.getElementById("france");
 var containerWidth = parentDiv.clientWidth;
 var containerHeight = parentDiv.clientHeight;
 
-  /*
-   * d3.cartogram is a d3-friendly implementation of An Algorithm to Construct
-   * Continuous Area Cartograms:
-   *
-   * <http://chrisman.scg.ulaval.ca/G360/dougenik.pdf>
-   *
-   * It requires topojson to decode TopoJSON-encoded topologies:
-   *
-   * <http://github.com/mbostock/topojson/>
-   *
-   * Usage:
-   *
-   * var cartogram = d3.cartogram()
-   *  .projection(d3.geo.albersUsa())
-   *  .value(function(d) {
-   *    return Math.random() * 100;
-   *  });
-   * d3.json("path/to/topology.json", function(topology) {
-   *  var features = cartogram(topology);
-   *  d3.select("svg").selectAll("path")
-   *    .data(features)
-   *    .enter()
-   *    .append("path")
-   *      .attr("d", cartogram.path);
-   * });
-   */
-
-  var svg = d3.select("#france")
+var svg = d3.select("#france")
     .append("svg")
         .attr("width", containerWidth - 10 )
         .attr("height", containerHeight - 10)
     .append("g")
-        .attr("height",  containerHeight - 10 )
+        .attr("height",  containerHeight - 10 );
 
-  var cartogram = d3.cartogram()
-      .projection()
-      .value(function(d) {
-      return Math.random() * 100;
+
+d3.json('../json/france.json', (error, world) => {
+    if (error) throw error;
+    const colorScale = d3.scaleOrdinal([...d3.schemeCategory20, ...d3.schemeCategory20b, ...d3.schemeCategory20c]);
+
+    Cartogram()
+        .topoJson(world)
+        .topoObjectName('countries')
+        .iterations(120);
+});
+
+
+// cartogram-chart
+
+const d3Cartogram = cartogramModule.cartogram; // Unwrap CJS from ES module
+
+const ANIMATION_DURATION = 1200;
+
+export default new Kapsule({
+
+  props: {
+    width: { default: window.innerWidth },
+    height: { default: window.innerHeight },
+    iterations: { default: 20 },
+    projection: { default: geoMercator()
+      .scale(Math.min((window.innerWidth - 3) / (2 * Math.PI), (window.innerHeight - 3) / (1.2 * Math.PI)))
+      .translate([window.innerWidth / 2, window.innerHeight / 1.5])
+    },
+    topoJson: {},
+    topoObjectName: {},
+    value: { default: 1 },
+    color: { default: 'lightgrey' },
+    label: { default: '' },
+    valFormatter: { default: n => n },
+    units: { default: '' },
+    tooltipContent: { default: d => '' },
+    onClick: { default: d => {} }
+  },
+
+  init(domNode, state) {
+    state.cartogram = d3Cartogram()
+      .properties(d => d.properties);
+
+    // Dom
+    state.svg = d3Select(domNode).append('svg')
+      .attr('class', 'cartogram');
+
+    // tooltips
+    state.tooltip = d3Select('body')
+      .append('div')
+      .attr('class', 'chart-tooltip cartogram-tooltip');
+
+    // tooltip cleanup on unmount
+    domNode.addEventListener ('DOMNodeRemoved', function(e) {
+      if (e.target === this) { state.tooltip.remove(); }
     });
 
-  d3.json("../json/france.json", function(topology) {
-      var features = cartogram(topology);
-      d3.select("svg").selectAll("path")
-          .data(features)
-          .enter()
-          .append("path")
-          .attr("d", cartogram.path);
-  });
+    state.svg.on('mousemove', () => {
+      state.tooltip
+        .style('left', d3Event.pageX + 'px')
+        .style('top', d3Event.pageY + 'px')
+        .style('transform', `translate(-${d3Event.pageX / state.width * 100}%, 21px)`); // adjust horizontal position to not exceed canvas boundaries
+    });
+  },
 
-  d3.cartogram = function() {
+  update(state) {
+    const valueOf = accessorFn(state.value);
+    const colorOf = accessorFn(state.color);
 
-    function carto(topology, geometries) {
-      // copy it first
-      topology = copy(topology);
+    state.svg
+      .attr('width', state.width)
+      .attr('height', state.height);
 
-      // objects are projected into screen coordinates
+    if (!state.topoJson) return; // No features to render
 
-      // project the arcs into screen space
-      var tf = transformer(topology.transform),x,y,len1,i1,out1,len2=topology.arcs.length,i2=0,
-          projectedArcs = new Array(len2);
-          while(i2<len2){
-            x = 0;
-            y = 0;
-            len1 = topology.arcs[i2].length;
-            i1 = 0;
-            out1 = new Array(len1);
-            while(i1<len1){
-              topology.arcs[i2][i1][0] = (x += topology.arcs[i2][i1][0]);
-              topology.arcs[i2][i1][1] = (y += topology.arcs[i2][i1][1]);
-              out1[i1] = projection(tf(topology.arcs[i2][i1]));
-              i1++;
-            }
-            projectedArcs[i2++]=out1;
-            
-          }
-
-      // path with identity projection
-      var path = d3.geo.path()
-        .projection(null);
-
-      var objects = object(projectedArcs, {type: "GeometryCollection", geometries: geometries})
-          .geometries.map(function(geom) {
-            return {
-              type: "Feature",
-              id: geom.id,
-              properties: properties.call(null, geom, topology),
-              geometry: geom
-            };
-          });
-
-      var values = objects.map(value),
-          totalValue = d3.sum(values);
-
-      // no iterations; just return the features
-      if (iterations <= 0) {
-        return objects;
-      }
-
-     var i = 0;
-      while (i++ < iterations) {
-        var areas = objects.map(path.area);
-            var totalArea = d3.sum(areas),
-            sizeErrorsTot =0,
-            sizeErrorsNum=0,
-            meta = objects.map(function(o, j) {
-              var area = Math.abs(areas[j]), // XXX: why do we have negative areas?
-                  v = +values[j],
-                  desired = totalArea * v / totalValue,
-                  radius = Math.sqrt(area / Math.PI),
-                  mass = Math.sqrt(desired / Math.PI) - radius,
-                  sizeError = Math.max(area, desired) / Math.min(area, desired);
-              sizeErrorsTot+=sizeError;
-              sizeErrorsNum++;
-              // console.log(o.id, "@", j, "area:", area, "value:", v, "->", desired, radius, mass, sizeError);
-              return {
-                id:         o.id,
-                area:       area,
-                centroid:   path.centroid(o),
-                value:      v,
-                desired:    desired,
-                radius:     radius,
-                mass:       mass,
-                sizeError:  sizeError
-              };
-            });
-
-        var sizeError = sizeErrorsTot/sizeErrorsNum,
-            forceReductionFactor = 1 / (1 + sizeError);
-
-        // console.log("meta:", meta);
-        // console.log("  total area:", totalArea);
-        // console.log("  force reduction factor:", forceReductionFactor, "mean error:", sizeError);
-
-        var len1,i1,delta,len2=projectedArcs.length,i2=0,delta,len3,i3,centroid,mass,radius,rSquared,dx,dy,distSquared,dist,Fij;
-        while(i2<len2){
-            len1=projectedArcs[i2].length;
-            i1=0;
-            while(i1<len1){
-              // create an array of vectors: [x, y]
-              delta = [0,0];
-              len3 = meta.length;
-              i3=0;
-              while(i3<len3) {
-                centroid =  meta[i3].centroid;
-                mass =      meta[i3].mass;
-                radius =    meta[i3].radius;
-                rSquared = (radius*radius);
-                dx = projectedArcs[i2][i1][0] - centroid[0];
-                dy = projectedArcs[i2][i1][1] - centroid[1];
-                distSquared = dx * dx + dy * dy;
-                dist=Math.sqrt(distSquared);
-                Fij = (dist > radius)
-                  ? mass * radius / dist
-                  : mass *
-                    (distSquared / rSquared) *
-                    (4 - 3 * dist / radius);
-                delta[0]+=(Fij * cosArctan(dy,dx));
-                delta[1]+=(Fij * sinArctan(dy,dx));
-                i3++;
-              }
-            projectedArcs[i2][i1][0] += (delta[0]*forceReductionFactor);
-            projectedArcs[i2][i1][1] += (delta[1]*forceReductionFactor);
-            i1++;
-          }
-          i2++;
-        }
-
-        // break if we hit the target size error
-        if (sizeError <= 1) break;
-      }
-
-      return {
-        features: objects,
-        arcs: projectedArcs
-      };
+    const topoObject = state.topoJson.objects[state.topoObjectName] || Object.values(state.topoJson.objects)[0];
+    if (!topoObject) {
+      console.warn('Unable to find topology object in TopoJson');
+      return;
     }
 
-    var iterations = 8,
-        projection = d3.geo.albers(),
-        properties = function(id) {
-          return {};
-        },
-        value = function(d) {
-          return 1;
-        };
+    state.cartogram
+      .projection(state.projection)
+      .value(valueOf);
 
-    // for convenience
-    carto.path = d3.geo.path()
-      .projection(null);
+    const features = state.svg.selectAll('path.feature')
+      .data(state.cartogram
+        .iterations(1) // Initialize new features non-distorted
+        (state.topoJson, topoObject.geometries).features
+      );
 
-    carto.iterations = function(i) {
-      if (arguments.length) {
-        iterations = i;
-        return carto;
-      } else {
-        return iterations;
-      }
-    };
+    features.exit().remove();
 
-    carto.value = function(v) {
-      if (arguments.length) {
-        value = d3.functor(v);
-        return carto;
-      } else {
-        return value;
-      }
-    };
+    const newFeatures = features.enter().append('path')
+      .attr('class', 'feature')
+      .style('fill', 'lightgrey')
+      .attr('d', state.cartogram.path)
+      .on('mouseover', (feature) => {
+        const valueOf = accessorFn(state.value);
+        const labelOf = accessorFn(state.label);
+        const tooltipContentOf = accessorFn(state.tooltipContent);
 
-    carto.projection = function(p) {
-      if (arguments.length) {
-        projection = p;
-        return carto;
-      } else {
-        return projection;
-      }
-    };
+        const label = labelOf(feature);
+        const extraContent = tooltipContentOf(feature);
+        state.tooltip.style('display', 'inline');
+        state.tooltip.html(`
+          ${label ? `<b>${label}</b>:` : ''}
+          ${state.valFormatter(valueOf(feature))}
+          ${state.units}
+          ${extraContent ? `<br/><br/>${extraContent}` : ''}
+        `);
+      })
+      .on('mouseout', () => { state.tooltip.style('display', 'none'); })
+      .on('click', d => state.onClick(d));
 
-    carto.feature = function(topology, geom) {
-      return {
-        type: "Feature",
-        id: geom.id,
-        properties: properties.call(null, geom, topology),
-        geometry: {
-          type: geom.type,
-          coordinates: topojson.object(topology, geom).coordinates
-        }
-      };
-    };
-
-    carto.features = function(topo, geometries) {
-      return geometries.map(function(f) {
-        return carto.feature(topo, f);
-      });
-    };
-
-    carto.properties = function(props) {
-      if (arguments.length) {
-        properties = d3.functor(props);
-        return carto;
-      } else {
-        return properties;
-      }
-    };
-
-    return carto;
-  };
-
-  var transformer = d3.cartogram.transformer = function(tf) {
-    var kx = tf.scale[0],
-        ky = tf.scale[1],
-        dx = tf.translate[0],
-        dy = tf.translate[1];
-
-    function transform(c) {
-      return [c[0] * kx + dx, c[1] * ky + dy];
-    }
-
-    transform.invert = function(c) {
-      return [(c[0] - dx) / kx, (c[1]- dy) / ky];
-    };
-
-    return transform;
-  };
-
-  function angle(a, b) {
-    return Math.atan2(b[1] - a[1], b[0] - a[0]);
+    features.merge(newFeatures)
+      .data(state.cartogram
+        .iterations(state.iterations) // distort all features
+        (state.topoJson, topoObject.geometries).features
+      )
+      .transition().duration(ANIMATION_DURATION)
+        .style('fill', colorOf)
+        .attr('d', state.cartogram.path);
   }
 
-  function distance(a, b) {
-    var dx = b[0] - a[0],
-        dy = b[1] - a[1];
-    return Math.sqrt(dx * dx + dy * dy);
-  }
+});
 
-  function projector(proj) {
-    var types = {
-      Point: proj,
-      LineString: function(coords) {
-        return coords.map(proj);
-      },
-      MultiLineString: function(arcs) {
-        return arcs.map(types.LineString);
-      },
-      Polygon: function(rings) {
-        return rings.map(types.LineString);
-      },
-      MultiPolygon: function(rings) {
-        return rings.map(types.Polygon);
-      }
-    };
-    return function(geom) {
-      return types[geom.type](geom.coordinates);
-    };
-  }
-  function cosArctan(dx,dy){
-    var div = dx/dy;
-    return (dy>0)?
-      (1/Math.sqrt(1+(div*div))):
-      (-1/Math.sqrt(1+(div*div)));
-  }
-  function sinArctan(dx,dy){
-    var div = dx/dy;
-    return (dy>0)?
-      (div/Math.sqrt(1+(div*div))):
-      (-div/Math.sqrt(1+(div*div)));
-  }
-  function copy(o) {
-    return (o instanceof Array)
-      ? o.map(copy)
-      : (typeof o === "string" || typeof o === "number")
-        ? o
-        : copyObject(o);
-  }
-  
-  function copyObject(o) {
-    var obj = {};
-    for (var k in o) obj[k] = copy(o[k]);
-    return obj;
-  }
-
-  function object(arcs, o) {
-    function arc(i, points) {
-      if (points.length) points.pop();
-      for (var a = arcs[i < 0 ? ~i : i], k = 0, n = a.length; k < n; ++k) {
-        points.push(a[k]);
-      }
-      if (i < 0) reverse(points, n);
-    }
-
-    function line(arcs) {
-      var points = [];
-      for (var i = 0, n = arcs.length; i < n; ++i) arc(arcs[i], points);
-      return points;
-    }
-
-    function polygon(arcs) {
-      return arcs.map(line);
-    }
-
-    function geometry(o) {
-      o = Object.create(o);
-      o.coordinates = geometryType[o.type](o.arcs);
-      return o;
-    }
-    var geometryType = {
-      LineString: line,
-      MultiLineString: polygon,
-      Polygon: polygon,
-      MultiPolygon: function(arcs) { return arcs.map(polygon); }
-    };
-
-    return o.type === "GeometryCollection"
-        ? (o = Object.create(o), o.geometries = o.geometries.map(geometry), o)
-        : geometry(o);
-  }
-
-  function reverse(array, n) {
-    var t, j = array.length, i = j - n; while (i < --j) t = array[i], array[i++] = array[j], array[j] = t;
-  }
